5f4e222b3dd46de45a39d1cb5f1422ac582f78f1
Andreas Haas <andy.haas.de@gmail.com>
2026-02-24 12:16:02 +0100

    initial commit mit erstes Aufgabe

---
+++ src/main/java/com/haas/andy/tag01/Variables.java
@@ -0,0 +1,26 @@
+package com.haas.andy.tag01;
+
+public class Variables {
+    public static void main(String[] args) {
+        /* Ü1 */
+        int year; // deklaration
+        year = 2026; // initialisation
+        System.out.println(year);
+
+        /* Ü2 */
+        double temperature = 10.3;
+        System.out.println(temperature);
+
+        /* Ü3 */
+        String city = "Berlin";
+        System.out.println(city);
+
+        /* Ü4 */
+        boolean isAvailable = false;
+        System.out.println(isAvailable);
+
+        /* Ü5 */
+        char grade = 'B';
+        System.out.println(grade);
+    }
+}
